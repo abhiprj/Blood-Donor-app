5cf203e2ad3fcbf1eb1dfaebe414b542dfeddbc8
Abhishek Prajapati <abhimrj8355@gmail.com>
2023-09-28 17:43:58 +0530

    updated

--- server.js
+++ server.js
@@ -26,13 +26,14 @@ app.use(morgan("dev"));
 //         message: 'welcome to blood bank app',
 //     })
 // })
+app.use(express.static(path.join(__dirname, "./client/build")));
+
 app.use("/api/v1/test", require("./routes/testRoutes"));
 app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
-app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
